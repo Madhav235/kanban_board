3ac83ac58c2a10817ae81c857d6a5cb40434f724
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-02-19 22:03:16 +0530

    hover functionality

--- home.js
+++ home.js
@@ -3,21 +3,50 @@ const todo = document.querySelector("#todo");
 const inprogress = document.querySelector("#inprogress");
 const done = document.querySelector("#done");
 const sections = document.querySelectorAll(".common");
+let dragElement = null;
 
 const tasks = document.querySelectorAll(".task");
 
 tasks.forEach((task) => {
     task.addEventListener("drag", (e) => {
         // console.log(e);
+        // console.log(task);
+        dragElement = task;
     })
 })
 
-sections.forEach((val) => {
-    val.addEventListener("dragenter", (e)=>{
-        val.classList.add("hover_over");
+// sections.forEach((val) => {
+//     val.addEventListener("dragenter", (e)=>{
+//         val.classList.add("hover_over");
+//     })
+
+//     val.addEventListener("dragleave", (e) => {
+//         val.classList.remove("hover_over");
+//     })
+// })
+
+function addDragEventsOnSections(columns){
+    columns.addEventListener("dragenter",(e)=>{
+        columns.classList.add("hover_over");
     })
 
-    val.addEventListener("dragleave", (e) => {
-        val.classList.remove("hover_over");
+    columns.addEventListener("dragleave",(e)=>{
+    columns.classList.remove("hover_over");
     })
+
+    columns.addEventListener("dragover",(e)=>{
+        e.preventDefault();
+    })
+
+    columns.addEventListener("drop",(e)=>{
+        e.preventDefault();
+        // console.log("dropped");
+        // console.log("dropped",dragElement,columns)
+        columns.appendChild(dragElement);
+        columns.classList.remove("hover_over");
+    })
+}
+
+sections.forEach((columns)=>{
+    addDragEventsOnSections(columns);
 })
