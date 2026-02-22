1f480d52d7a02072ea35e896b15e94a64b4a08da
Madhav Agarwal <madhavagarwal369@gmail.com>
2026-02-23 01:29:55 +0530

    complete local storage accessibiliy

--- home.js
+++ home.js
@@ -4,105 +4,124 @@ const todo = document.getElementById("todo");
 const inprogress = document.getElementById("inprogress");
 const done = document.getElementById("done");
 
-// create previous information if possible
+// create previous information if present
 
-if (localStorage.getItem("tasks")){
-    const data= JSON.parse(localStorage.getItem("tasks"));
+if (localStorage.getItem("tasks")) {
+    const data = JSON.parse(localStorage.getItem("tasks"));
 
-    for (const a in data){
-        const column= document.querySelector(`#${a}`);
-        data[a].forEach    
+    for (const a in data) {
+        const column = document.querySelector(`#${a}`);
+        data[a].forEach((task) => {
+            let div = document.createElement("div");
+            let name = document.createElement("div");
+            let desc = document.createElement("div");
+            let deleteButton = document.createElement("button");
+            div.classList.add("task");
+            name.classList.add("taskName");
+            desc.classList.add("taskDescription");
+            deleteButton.classList.add("delete");
+            deleteButton.innerText = "Delete"
+            div.appendChild(name);
+            div.appendChild(desc);
+            div.appendChild(deleteButton);
+            div.setAttribute("draggable", "true");
+            name.innerText = task.title;
+            desc.innerText = task.description;
+            column.appendChild(div);
+            deleteTaskTrack();
+        });
     }
 }
 
+updateCount();
 // dashed hover effect and drag and drop functionality
 
-const task= document.querySelectorAll(".task");
-let draggedElement= null;
+const task = document.querySelectorAll(".task");
+let draggedElement = null;
 
-function elementDragged(val){
-    val.addEventListener("dragstart",(e)=>{
-        draggedElement= val;
+function elementDragged(val) {
+    val.addEventListener("dragstart", (e) => {
+        draggedElement = val;
     });
 };
 
-task.forEach((e)=>{
+task.forEach((e) => {
     elementDragged(e);
 });
 
-const sections= document.querySelectorAll(".common");
+const sections = document.querySelectorAll(".common");
 
-function dragAndDropFunctionality(val){
-    val.addEventListener("dragenter",(e)=>{
+function dragAndDropFunctionality(val) {
+    val.addEventListener("dragenter", (e) => {
         val.classList.add("hover_over");
     });
 
-    val.addEventListener("dragleave",(e)=>{
+    val.addEventListener("dragleave", (e) => {
         val.classList.remove("hover_over");
     });
 
-    val.addEventListener("dragover",(e)=>{
+    val.addEventListener("dragover", (e) => {
         e.preventDefault();
     });
 
-    val.addEventListener("drop",(e)=>{
+    val.addEventListener("drop", (e) => {
         e.preventDefault();
         val.appendChild(draggedElement);
         val.classList.remove("hover_over");
         updateCount();
-        addToLocalStorage(val);
+        addToLocalStorage();
     });
 };
 
-sections.forEach((e)=>{
+sections.forEach((e) => {
     dragAndDropFunctionality(e);
 });
 
 
 // add new task modal visibility
 
-const addNewTask= document.querySelector(".add");
-const modal= document.querySelector(".modal");
-const modalExit= document.querySelector(".modal_exit");
+const addNewTask = document.querySelector(".add");
+const modal = document.querySelector(".modal");
+const modalExit = document.querySelector(".modal_exit");
 
-addNewTask.addEventListener("click",(e)=>{
+addNewTask.addEventListener("click", (e) => {
     modal.classList.add("active");
 });
 
-modalExit.addEventListener("click",(e)=>{
+modalExit.addEventListener("click", (e) => {
     modal.classList.remove("active");
 });
 
 // add new task to todo
 
-const taskTitle= document.querySelector(".task_name");
-const taskDescription= document.querySelector(".task_desc");
-const addButton= document.querySelector(".add_btn");
+const taskTitle = document.querySelector(".task_name");
+const taskDescription = document.querySelector(".task_desc");
+const addButton = document.querySelector(".add_btn");
 
-addButton.addEventListener("click",(e)=>{
+addButton.addEventListener("click", (e) => {
     // extracting user values
 
-    let title= taskTitle.value;
-    let description= taskDescription.value;
+    let title = taskTitle.value;
+    let description = taskDescription.value;
 
     // creating task element dynamically
 
-    let div= document.createElement("div");
-    let name= document.createElement("div");
-    let desc= document.createElement("div");
-    let deleteButton= document.createElement("button");
-    const todo= document.querySelector(".todo");
+    let div = document.createElement("div");
+    let name = document.createElement("div");
+    let desc = document.createElement("div");
+    let deleteButton = document.createElement("button");
+    const todo = document.querySelector(".todo");
     div.classList.add("task");
     name.classList.add("taskName");
     desc.classList.add("taskDescription");
     deleteButton.classList.add("delete");
-    deleteButton.innerText= "Delete"
+    deleteButton.innerText = "Delete"
     div.appendChild(name);
     div.appendChild(desc);
     div.appendChild(deleteButton);
-    div.setAttribute("draggable","true");
-    name.innerText= title;
-    desc.innerText= description;
+    div.setAttribute("draggable", "true");
+    name.innerText = title;
+    desc.innerText = description;
 
     // appending to new element
 
@@ -125,64 +144,66 @@ addButton.addEventListener("click",(e)=>{
 
 // update count
 
-function updateCount(){
-    const todoCount= todo.querySelectorAll(".task");
-    const inProgressCount= inprogress.querySelectorAll(".task");
-    const doneCount= done.querySelectorAll(".task");
-    const count= document.querySelectorAll(".count");
-    count[0].innerText= todoCount.length; 
-    count[1].innerText= inProgressCount.length;
-    count[2].innerText= doneCount.length;
+function updateCount() {
+    const todoCount = todo.querySelectorAll(".task");
+    const inProgressCount = inprogress.querySelectorAll(".task");
+    const doneCount = done.querySelectorAll(".task");
+    const count = document.querySelectorAll(".count");
+    count[0].innerText = todoCount.length;
+    count[1].innerText = inProgressCount.length;
+    count[2].innerText = doneCount.length;
 }
 
 // delete button functionality
 
-function deleteTaskTrack(){
-    const todo= document.querySelector(".todo");
-    const inProgress= document.querySelector(".inprogress");
-    const done= document.querySelector(".done");
-    const todoDelete= todo.querySelectorAll(".delete");
-    const inProgressDelete= inProgress.querySelectorAll(".delete");
-    const doneDelete= done.querySelectorAll(".delete");
-    todoDelete.forEach((e)=>{
+function deleteTaskTrack() {
+    const todo = document.querySelector(".todo");
+    const inProgress = document.querySelector(".inprogress");
+    const done = document.querySelector(".done");
+    const todoDelete = todo.querySelectorAll(".delete");
+    const inProgressDelete = inProgress.querySelectorAll(".delete");
+    const doneDelete = done.querySelectorAll(".delete");
+    todoDelete.forEach((e) => {
         deleteButtonFunctionality(e);
     });
 
-    inProgressDelete.forEach((e)=>{
+    inProgressDelete.forEach((e) => {
         deleteButtonFunctionality(e);
     });
 
-    doneDelete.forEach((e)=>{
+    doneDelete.forEach((e) => {
         deleteButtonFunctionality(e);
     });
 };
 
-function deleteButtonFunctionality(val){
-    val.addEventListener("click",(e)=>{
-        let taskToRemove= val.parentElement;
-        let taskColumnToRemove= taskToRemove.parentElement;
+function deleteButtonFunctionality(val) {
+    val.addEventListener("click", (e) => {
+        let taskToRemove = val.parentElement;
+        let taskColumnToRemove = taskToRemove.parentElement;
         taskColumnToRemove.removeChild(taskToRemove);
         updateCount();
+        addToLocalStorage();
     });
 };
 
 // local storage connection
 
-let taskData={}
+let taskData = {}
 
-function addToLocalStorage(val){
-    const tasks= document.querySelectorAll(".task");
-    taskData[val.id]= Array.from(tasks).map((e)=>{
-        return  {
-            title: e.querySelector(".taskName"),
-            description: e.querySelector(".taskDescription")
-        }
-    });
+function addToLocalStorage() {
 
-    localStorage.setItem("tasks",JSON.stringify(taskData));
-    console.log(taskData);
-};
+    const data = {
+        todo: getTasks(todo),
+        inprogress: getTasks(inprogress),
+        done: getTasks(done)
+    };
 
-sections.forEach((e)=>{
-    addToLocalStorage(e);
-});
+    localStorage.setItem("tasks", JSON.stringify(data));
+}
+
+function getTasks(section){
+    return Array.from(section.querySelectorAll(".task")).map(task => ({
+        title: task.querySelector(".taskName").textContent,
+        description: task.querySelector(".taskDescription").textContent
+    }));
+}
